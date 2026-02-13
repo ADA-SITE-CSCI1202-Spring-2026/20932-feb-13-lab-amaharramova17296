af058df07d63ef636ab9919271e2ac95a5e64c86
Ayan Maharramova <amaharramova17296@ada.edu.az>
2026-02-13 11:19:27 +0400

    Add files via upload

---
+++ week03/invoice/Invoice.java
@@ -0,0 +1,48 @@
+package week03.invoice;
+
+public class Invoice {
+    String partNum;
+    String partDescription;
+    int quantity;
+    double pricePerItem;
+
+     public Invoice(String partNum, String partDescription, int quantity, double pricePerItem) {
+        this.partNum = partNum;
+        this.partDescription = partDescription;
+        this.quantity = quantity;
+        this.pricePerItem = pricePerItem;
+    }
+    public String getNum() {
+        return partNum;
+    }
+
+    public void setNum(String partNum) {
+        this.partNum = partNum;
+    }
+    
+    public String getDescription() {
+          return partDescription;
+    }
+
+    public void SetDescription(String partDescription) {
+        this.partDescription = partDescription;
+    }
+    
+     public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return pricePerItem;
+    }
+
+    public void setPrice(double pricePerItem) {
+        this.pricePerItem = pricePerItem;
+    }
+
+
+}
